a4ac1b8d35192aae36cb0e8b3c4c00efd3871af2
oktawiahankus <163405012+oktawiahankus@users.noreply.github.com>
2024-12-12 22:28:58 +0100

    data adjustment

--- R/data_setup.R
+++ R/data_setup.R
@@ -1,6 +1,11 @@
-saveRDS(data, file = "japan_dt.RDS")
-dt = readRDS(file = "japan_dt.RDS")
-tail(dt[,1])
-
+# data is a table imported from data.txt using 'Import Dataset'
 # Japan National Tourism Organization (JNTO)
-japan_dt
+saveRDS(data, file = "data/raw_data.RDS")
+raw_data <- readRDS("data/raw_data.RDS")
+
+japan_dt <- as.data.table(raw_data)
+japan_dt[, Growth.Rate... := NULL]
+colnames(japan_dt) <- c("country", "month", "year", "visitors")
+
+
+saveRDS(japan_dt, file = "data/japan_dt.RDS")
---
+++ R/setup.R
@@ -0,0 +1,3 @@
+library(data.table)
+library(ggplot2)
+library(forecast)
