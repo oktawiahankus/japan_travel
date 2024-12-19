95bf0892c28372d9362e858efad03d581d56e49c
oktawiahankus <163405012+oktawiahankus@users.noreply.github.com>
2024-12-19 12:55:25 +0100

    chyba wszystko działa

--- R/data_setup.R
+++ R/data_setup.R
@@ -3,6 +3,7 @@
 saveRDS(data, file = "data/raw_data.RDS")
 raw_data <- readRDS("data/raw_data.RDS")
 
+
 japan_dt <- as.data.table(raw_data)
 japan_dt[, Growth.Rate... := NULL]
 colnames(japan_dt) <- c("country", "month", "year", "visitors")
@@ -14,6 +15,7 @@ japan_dt <- japan_dt[, .(visitors = sum(visitors, na.rm = TRUE)), by = date]
 # deleting lack of data, probably not gathered yet
 japan_dt <- japan_dt[visitors > 0]
 saveRDS(japan_dt, file = "data/japan_dt.RDS")
+japan_dt <- readRDS("data/japan_dt.RDS")
 
 japan_zoo <- zoo(japan_dt$visitors, japan_dt$date)
 japan_ts <- ts(japan_zoo, start = 1990, frequency = 12)
@@ -22,3 +24,8 @@ plot(japan_ts)
 # jest ok, czy nie, ale chyba inaczej się nie da, bo nie wczytywało automatcznie
 # frequency - the number of observations per unit of time, 
 # czyli by się zgadzało 
+
+
+plot_ly(japan_dt[order(japan_dt$date), ], x = ~date, y = ~visitors, type = 'scatter', mode = 'lines')
+
+# można to jeszcze rozbić na dane ze względu na kraje, skąd turysta
