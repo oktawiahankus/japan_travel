2d5232a46c2aa340d40e8719206bc999d02cca69
Magdalena Aleksandra Potok <328821@uwr.edu.pl>
2025-01-17 16:00:41 +0100

    rysunki model2012-24

--- R/model_2012_2024_wersjaczysta.R
+++ R/model_2012_2024_wersjaczysta.R
@@ -50,7 +50,36 @@ test = window(japan_filtered, start=c(2023, 11))
 ?date_format
 ts_plot(test)
 ts_plot(train)
-ts_decompose(train, type = "multiplicative") 
+
+# decompose od julki
+decomposed_multiplicative = decompose(train, type = "multiplicative") 
+decomposed_data <- data.frame(
+  czas = rep(time(train), 4),
+  wartość = c(
+    decomposed_multiplicative$x,
+    decomposed_multiplicative$trend,
+    decomposed_multiplicative$seasonal,
+    decomposed_multiplicative$random
+  ),
+  komponent = factor(
+    rep(c("Oryginalne dane", "Trend", "Sezonowość", "Reszty"), each = length(train)),
+    levels = c("Oryginalne dane", "Trend", "Sezonowość", "Reszty")
+  )
+)
+ggplot(decomposed_data, aes(x = czas, y = wartość)) +
+  geom_line() +
+  facet_wrap(~ komponent, ncol = 1, scales = "free_y") +
+  labs(
+    title = "Dekompozycja szeregu (multiplikatywna)",
+    x = "Czas",
+    y = "Wartość"
+  ) +
+  theme_minimal() +
+  theme(
+    strip.text = element_text(size = 12, face = "bold"),
+    plot.title = element_text(size = 14, face = "bold", hjust = 0.5)
+  )
+
 # on dla airpassengers wybral multiplikatywna, wiec tez tak zrobimy moze -- metoda analogicznego podejscia do wykladu
 #1. widac ze moment pandemii zaburza nam szereg, trend bylby staly jak w latach 2012-2019, ale covid to psuje
 # 2. widac sezonowosc, w ciagu roku mamy 3 maksima lokalne?
@@ -126,8 +155,8 @@ diff_train
 
 
 par(mfrow = c(1, 2)) # Dwa wykresy obok siebie
-Acf(diff_train, main="ACF różnicowanych danych", lag = 24) # wg mnie MA(q) q = 4 albo 6 nwm 
-Pacf(diff_train, main="PACF różnicowanych danych", lag = 24) 
+Acf(diff_train, main="ACF zróżnicowanych danych", lag = 24) # wg mnie MA(q) q = 4 albo 6 nwm 
+Pacf(diff_train, main="PACF zróżnicowanych danych", lag = 24) 
 
 
 auto.arima(train) #q = 1, p = 1 ok
@@ -148,8 +177,8 @@ summary(model_arima616)
 summary(model_season) 
 summary(model_ets)
 
-AIC(model_auto, model_arima616, model_sarima616, model_sarima114,model_sarima116) #auto model lepszy, ale nie duzo tbh
-BIC(model_auto, model_arima616, model_sarima616, model_sarima114,model_sarima116) #znowu nieduzo lepszy
+round(AIC(model_auto, model_arima616, model_sarima616, model_sarima114,model_sarima116),2) #auto model lepszy, ale nie duzo tbh
+round(BIC(model_auto, model_arima616, model_sarima616, model_sarima114,model_sarima116),2) #znowu nieduzo lepszy
 # wybieramy 3 najlepsze na podstawie AIC i BIC
 # model_auto, model_arima616, model_sarima114
 
@@ -170,9 +199,9 @@ qqline(residuals_auto)
 qqnorm(residuals_arima)
 qqline(residuals_arima)
 par(mfrow = c(1,3))
-Acf(residuals_auto)
-Acf(residuals_arima616) # ups tu wystaje xd
-Acf(residuals_sarima114)
+Acf(residuals_arima616, main = "ARIMA(6,1,6)") # ups tu wystaje xd
+Acf(residuals_sarima114, main = "SARIMA(1,1,4)(2,0,1)[12]")
+Acf(residuals_auto, main = "SARIMA(1,1,1)(2,0,1)[12]")
 
 
 
@@ -184,7 +213,7 @@ forecast_sarima114 = forecast(model_sarima114, level = c(95), h = length(test))
 model_sarima = sarima.for(train, n.ahead = length(test), p = 1, d = 1, q = 1, P = 1, D = 0, Q = 2, S = 12) # wartosci wspolczynnikow wybralam z model_auto (auto.arima)
 
 data = data.frame(
-  czas=time(test),
+  czas=as.Date(time(test)),
   test,
   forecast_auto$mean,
   forecast_arima616$mean, 
@@ -200,17 +229,32 @@ ggplot(data, aes(x = czas)) +
   labs(x = 'Czas', y = 'Wartość', color = 'Legenda') +
   scale_color_manual(values = c('Dane testowe' = 'black', 'SARIMA(1,1,1)(1,0,2)_{12}' = 'hotpink', 'eARIMA(1,1,4)' = 'blue', 'SSarima' = 'darkviolet'),
                      labels = c('Dane testowe' = 'Dane testowe', 
-                                'SARIMA(1,1,1)(1,0,2)_{12}' = expression(SARIMA(1,1,1)(1,0,2)[12]), 
-                                'eARIMA(1,1,4)' = expression(ARIMA(1,1,4)),
-                                 'SSarima' = expression(SARIMA(1,1,4)(1,0,2)[12])))
+                                'SARIMA(1,1,1)(1,0,2)_{12}' = expression(SARIMA(1,1,1)(2,0,1)[12]), 
+                                'eARIMA(1,1,4)' = expression(ARIMA(6,1,6)),
+                                 'SSarima' = expression(SARIMA(1,1,4)(2,0,1)[12])))+
+  scale_x_date(date_breaks = "1 month", 
+               limits = as.Date(c('2023-11-01','2024-08-01')),
+               date_labels = "%Y %b")
+
 # SARIMA calkiem niezle!
 # ARIMA ???slabo
 
 # jakies statystyki 
-sum((test - forecast_auto$mean)^2)
-sum((test - forecast_arima$mean)^2) # okur ale duzoxd
-sum((test - model_sarima$pred)^2) # ten ma najmniej ale nie wiem, to ten sam model tylko pewnie jakis inny sposob dobierania tych wspolczynnikow
 
+#MSE
+MSE_auto = mean((test - forecast_auto$mean)^2)
+MSE_arima616 = mean((test - forecast_arima616$mean)^2) # okur ale duzoxd
+MSE_sarima114 = mean((test - forecast_sarima114$mean)^2) # ten ma najmniej ale nie wiem, to ten sam model tylko pewnie jakis inny sposob dobierania tych wspolczynnikow
+
+#RMSD sqrt(mean((true - forecast)^2))
+sqrt(MSE_auto)
+sqrt(MSE_arima616)
+sqrt(MSE_sarima114)
+
+#MAE mean absolute error sum
+mean(abs(test - forecast_auto$mean))
+mean(abs(test - forecast_arima616$mean))
+mean(abs(test - forecast_sarima114$mean))
 
 # Analiza spektralna 
 # sluzy do okreslenia s 
